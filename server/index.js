ea86b7af17138f37e935caa20bb2325d5bea2f95
krishna agarwal <79649114+krishnaagarwal1506@users.noreply.github.com>
2023-04-07 10:53:33 +0530

    changes in server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+
 import dalleRoutes from "./routes/dalle.routes.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,9 +13,7 @@ app.use(express.json({ limig: "50mb" }));
 app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World" });
+  res.status(200).json({ message: "Hello from DALL.E" });
 });
 
-app.listen(8080, () => {
-  console.log("server is started");
-});
+app.listen(8080, () => console.log("Server has started on port 8080"));
